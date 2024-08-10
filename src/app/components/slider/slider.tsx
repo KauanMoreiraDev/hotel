8a0441df27e2d429d6a293fb71a9d16b49329983
KauanMoreiraDev <kauanmoreira457@gmail.com>
2024-08-10 02:55:11 -0300

    Update slider.tsx

--- src/app/components/slider/slider.tsx
+++ src/app/components/slider/slider.tsx
@@ -3,6 +3,7 @@
 import Data from "@/app/components/data/data.json"
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 
 const handleClick = () => {
     console.log('você entrou em contato')
@@ -31,16 +32,15 @@ const Slider = () => {
     };
 
     return (
-        <div className="relative mx-auto overflow-hidden w-screen h-[80vh]">
+        <div className="relative mx-auto overflow-hidden w-screen min-h-[80vh]">
             <div className="flex transition-transform ease-in-out duration-500" style={{ transform: `translateX(-${current * 100}%)` }}>
                 {photos.map((photos, index) => (
                     <Image key={index} src={photos.url} alt={`Slide ${index}`} width={1500} height={1500} className="w-full h-full object-cover flex-shrink-0" />
                 ))}
             </div>
-            <button onClick={prevSlide} className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-800 text-white p-2 w-1/2 h-screen opacity-10" />
-            <button onClick={nextSlide} className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-800 text-white p-2 w-1/2 h-screen opacity-10" />
+            <button onClick={prevSlide} className="absolute top-1/2 left-0 transform -translate-y-1/2 p-2 w-1/12 h-screen opacity-100"><ChevronLeftIcon className="text-white" /></button>
+            <button onClick={nextSlide} className="absolute top-1/2 right-0 transform -translate-y-1/2 p-2 w-1/12 h-screen opacity-100"><ChevronRightIcon className="text-white" /></button>
         </div>
-
     );
 };
 
