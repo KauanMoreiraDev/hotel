1f8dd04cef186dceb9b85132262e4ef5c05282ee
KauanMoreiraDev <kauanmoreira457@gmail.com>
2024-08-15 12:05:46 -0300

    update altura card

--- src/app/components/cardSimple/cardsimple.tsx
+++ src/app/components/cardSimple/cardsimple.tsx
@@ -12,7 +12,7 @@ const CardSimple = ({ background = "bg-blue-300", width = "w-full",img, label = 
         <div className={`${background} shadow-xl rounded-3xl p-2 m-2 h-full ${width}`}>
           <div className="flex flex-col items-center">
             <Image src={"/images/logo 1.png"} alt="aafa" width={200} height={200} className="w-1/2 h-1/2 p-1 m-1" />
-            <p className="text-lg w-1/2 h-14 text-center">{label}</p>
+            <p className="text-lg w-1/2 h-1/2 text-center">{label}</p>
           </div>
         </div>
     );
