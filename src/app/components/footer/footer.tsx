c31968db690818bcd44011814577c737745e1ff9
KauanMoreiraDev <kauanmoreira457@gmail.com>
2024-08-19 00:42:36 -0300

    tentando acertar slider depoimentos

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -22,34 +22,33 @@ const Footer = ({
     return (
         <footer className={`${background} ${textColor} ${wSize} ${hSize}`} >
             <div className="mx-auto max-w-screen-lg min-h-full p-5">
-                        <div className="flex flex-col items-start sm:flex-row sm:space-x-12">
-                            <div className="flex flex-col items-center sm:items-start text-black font-medium">
-                                <Image src={logo.logoPadrão.src} alt={logo.logoPadrão.alt} width={350} height={350} className="mx-1" />
-                                <p>Av. Rodolpho Morelli, 2016</p>
-                                <p>Zona Rural, Santa Cruz da Conceição - SP</p>
-                                <p> CEP-13629-022</p>
-                            </div>
-                            <div className="flex flex-col items-start p-2">
-                                {navLinks.map((navfn, index) => (
-                                    <Link key={index} href={navfn.url} className=" m-1">{navfn.label}</Link>
-                                ))}
-                            </div>
-                            <div className="flex flex-col items-start p-2">
-                                {institucional.map((institucional, index) => (
-                                    <Link key={index} href={institucional.url} target="_blank" className=" m-1">{institucional.label}</Link>
-                                ))}
-                            </div>
-                        </div>
+                <div className="flex flex-col items-start sm:flex-row sm:space-x-12">
+                    <div className="flex flex-col items-center sm:items-start text-black font-medium">
+                        <Image src={logo.logoPadrão.src} alt={logo.logoPadrão.alt} width={350} height={350} className="mx-1" />
+                        <p>Av. Rodolpho Morelli, 2016</p>
+                        <p>Zona Rural, Santa Cruz da Conceição - SP</p>
+                        <p> CEP-13629-022</p>
                     </div>
-                    <div className="flex flex-col items-center p-3">
-                            <div className="flex flex-col items-start p-2">
-                                <p>(19) 97124-0437</p>
-                                <RedesSociais sizeIcon="text-3xl" sizeIconSm="text-xl"/>
-                            </div>
-                        <p>Todos os direitos reservados 2024</p>
-                        <Link href="https://portfoliokdev.netflify.app" target="_blank" className="font-medium text-lg text-black hover:text-violet-700">Desenvolvido por Kdev</Link>
+                    <div className="flex flex-col items-start p-2">
+                        {navLinks.map((navfn, index) => (
+                            <Link key={index} href={navfn.url} className=" m-1">{navfn.label}</Link>
+                        ))}
                     </div>
-                
+                    <div className="flex flex-col items-start p-2">
+                        {institucional.map((institucional, index) => (
+                            <Link key={index} href={institucional.url} target="_blank" className=" m-1">{institucional.label}</Link>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col items-center p-3">
+                <p>(19) 97124-0437</p>
+                <RedesSociais sizeIcon="text-3xl" sizeIconSm="text-xl" />
+
+                <p>Todos os direitos reservados 2024</p>
+                <Link href="https://portfoliokdev.netflify.app" target="_blank" className="font-medium text-lg text-black hover:text-violet-700">Desenvolvido por Kdev</Link>
+            </div>
+
         </footer>
     );
 }
--- src/app/components/slideDepoimentos/slider.tsx
+++ src/app/components/slideDepoimentos/slider.tsx
@@ -71,37 +71,25 @@ const Slider = ({ rounded, width = "w-full", smWidth = "w-full", className }: Sl
     };
 
     return (
-        <div className={`relative overflow-hidden ${width} ${smWidth} h-full ${rounded} py-16 ${className} flex flex-row items-center justify-center`}>
+        <div className={`relative overflow-hidden ${width} ${smWidth} h-full ${rounded} py-16 `}>
 
-            <button onClick={prevSlide} className="p-2 mx-2 flex justify-start items-center rounded-full bg-slate-500"><ChevronLeftIcon className="text-white" /></button>
+            <button onClick={prevSlide} className=" p-2 mx-2 flex justify-start items-center rounded-full bg-slate-500"><ChevronLeftIcon className="text-white" /></button>
 
             {/* perguntar para o gu sobre isso, comparação com o de baixo <div className={`flex transition-transform ease-in-out duration-500 transform:translate-x-${current * 100/itemsToShow}%`}> */}
-            {/* 
-            <div className="flex transition-transform ease-in-out duration-500" style={{ transform: `translateX(-${current * 100}%)` }}> */}
 
-            <div className={`relative flex flex-col items-start justify-items-center w-11/12 h-full p-12 rounded-3xl ${current}`}>
-                <div className="absolute inset-0 rounded-3xl bg-slate-300 backdrop-blur-sm" />
-                {labels.map((labels, index) => (
-                    <div key={index} className="relative flex flex-col items-start justify-items-center w-full h-full">
-
-                        <div className="flex flex-row justify-start">
-                            {Array.from({length: labels.ranking}).map((_, index) => (
-                                <StarFilledIcon key={index} className="text-yellow-400 w-6 h-6" />
-
-                            ))}
-                            
-                        </div>
-
-                        <div className="text-lg flex flex-row">
-                            <PersonIcon className="w-1/2 h-1/2" />
-                            {labels.nome}
-                        </div>
-                        <p className="py-5 h-16">{labels.label}</p>
+            <div className="flex transition-transform ease-in-out duration-500" style={{ transform: `translateX(-${current * 100}%)` }}>
+                {labels.map((items, index) => (
+                    <div key={index} className="w-full h-full object-cover flex-shrink-0 ">
+                        <div className="p-2 flex">{Array.from({ length: items.ranking}).map((_, index) => (
+                            <StarFilledIcon key={index} className="text-yellow-400 w-10 h-10" />
+                        ))}</div>
+                        <div className="p-2 text-lg flex items-center"><PersonIcon className="w-10 h-10"/>{items.nome}</div>
+                        <div className="p-2">{items.label}</div>
                     </div>
                 ))}
             </div>
 
-            <button onClick={nextSlide} className="p-2 mx-2 flex justify-start items-center rounded-full bg-slate-500">
+            <button onClick={nextSlide} className=" p-2 mx-2 flex justify-start items-center rounded-full bg-slate-500">
                 <ChevronRightIcon className="text-white" />
             </button>
         </div>
