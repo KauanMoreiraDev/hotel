5b5194ee445580c1b5ef8ce9e516350c69676b57
KauanMoreiraDev <kauanmoreira457@gmail.com>
2024-08-22 15:59:12 -0300

    v1 100% ok

--- src/app/components/cardQuartos/cardQuartos.tsx
+++ src/app/components/cardQuartos/cardQuartos.tsx
@@ -3,8 +3,9 @@
 import Image from "next/image";
 import Button from "../button/button";
 import SliderImages from "../sliderImages/slider";
+import { LucideBed, LucideRefrigerator, LucideBath, LucideTv, LucideGroup, LucideWifi } from "lucide-react";
 
-interface CardProps{
+interface CardProps {
     backgroundColor?: string;
     hover?: string;
     srcImage?: string;
@@ -16,22 +17,38 @@ interface CardProps{
     onclick: () => void
 }
 
-const CardQuartos = ({ backgroundColor = "bg-slate-400", hover = "bg-slate-600", srcImage, titleFont, titleColor, titleLabel = "Titulo aqui",label = "escreva seu texto aqui", colorButoon = "bg-white", onclick } : CardProps) => {
+const handleClick = () => {
+    console.log("Function not implemented.")
+}
 
-    return(
-                    <div className={`w-full h-full p-2 m-3`}>
-                        <div className="flex flex-row items-center h-4/5">
-                            <div className="w-1/2">
-                                <SliderImages constSrc="photosChalé"/>
-                                {/* <Image src={`/images/chalé.png`} alt="teste" width={500} height={500} className="h-4/5 rounded-r-[90px] rounded-3xl z-20" /> */}
-                            </div>
-                            <div className={`${backgroundColor} w-full h-full pl-24 -ml-20 p-10 rounded-r-3xl`}>
-                                <p className={`${titleFont} text-5xl ${titleColor} mb-2 pb-1`}>{titleLabel}</p>
-                                    <p className="mb-2 pb-1 text-base min-h-[5em]">{label}</p>
-                                <Button onClick={onclick} textColor="text-white" textSize="text-lg" label={"Saiba mais"} wSize="w-full" background={`${colorButoon}`} backgroundHover={`${hover}`} />
-                            </div>
+const CardQuartos = ({ backgroundColor = "bg-slate-400", hover = "bg-slate-600", srcImage, titleFont, titleColor, titleLabel = "Titulo aqui", label = "escreva seu texto aqui", colorButoon = "bg-white", onclick }: CardProps) => {
+
+    {/* <Image src={`/images/chalé.png`} alt="teste" width={500} height={500} className="h-4/5 rounded-r-[90px] rounded-3xl z-20" /> */ }
+
+    return (
+        <div className="my-5">
+            <div className="flex flex-row justify-center">
+                <div className="w-5/12">
+                    <SliderImages constSrc="photosChale" sizeQuarto="h-96" rounded="rounded-3xl rounded-r-[75px]" />
+                </div>
+                <div className={`${backgroundColor} w-3/5 h-96 p-5 pl-24 -ml-20 rounded-3xl`}>
+                    <div className="flex flex-col justify-between h-full">
+                        <div className={`text-5xl ${titleColor} ${titleFont}`}>{titleLabel}</div>
+                        <div className="flex flex-row flex-wrap items-center h-[10%]">
+                            <div className="h-[7%] py-2 pr-3 text-xs flex flex-nowrap"><LucideWifi className="w-4 h-4 " />Wi-fi</div>
+                            <div className="h-[7%] py-2 pr-3 text-xs flex flex-nowrap"><LucideRefrigerator className="w-4 h-4 " />Frigobar</div>
+                            <div className="h-[7%] py-2 pr-3 text-xs flex flex-nowrap"><LucideBed className="w-4 h-4 " />Cama box</div>
+                            <div className="h-[7%] py-2 pr-3 text-xs flex flex-nowrap"><LucideBath className="w-4 h-4 " />Banheiro</div>
+                            <div className="h-[7%] py-2 pr-3 text-xs flex flex-nowrap"><LucideTv className="w-4 h-4 " />Tv via satélite</div>
+                            <div className="h-[7%] py-2 pr-3 text-xs flex flex-nowrap"><LucideBed className="w-4 h-4 " />2 Quartos</div>
+                            <div className="h-[7%] py-2 pr-3 text-xs flex flex-nowrap"><LucideGroup className="w-4 h-4 " />Máx 7 pessoas</div>
                         </div>
+                        <div className="">{label}</div>
+                        <Button onClick={onclick} background={colorButoon} backgroundHover={hover} label={"Reservar quarto"} wSize="w-full" hSize="h-1/6" />
                     </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/app/pages/acomodacoes/page.tsx
+++ src/app/pages/acomodacoes/page.tsx
@@ -12,8 +12,15 @@ const LadingPage = () => {
     }
 
     return (
-        <main className="mx-auto max-w-screen-lg">
-            <CardQuartos onclick={handleClick} />
+        <main className="mx-auto max-w-screen-lg ">
+            <CardQuartos onclick={handleClick} titleFont="font-Caveat" titleColor="bg-blue-400" titleLabel="Chalé" backgroundColor="bg-blue-400" label="Chalé suíço exclusivo, perfeito para até 7 pessoas, com opção de camas extras. Combine conforto rústico com uma vista deslumbrante e momentos inesquecíveis."/>
+
+            <CardQuartos onclick={handleClick} titleFont="font-Caveat" titleLabel="Suíte" backgroundColor="bg-blue-400" label="Desfrute do conforto e elegância da nossa suíte, projetada para proporcionar momentos inesquecíveis. Relaxe em um ambiente sofisticado, com todas as comodidades que você merece."/>
+
+            <CardQuartos onclick={handleClick} titleFont="font-Caveat" titleLabel="Palestras" backgroundColor="bg-blue-400" label="Desfrute do conforto e elegância da nossa suíte, projetada para proporcionar momentos inesquecíveis. Relaxe em um ambiente sofisticado, com todas as comodidades que você merece."/>
+
+            <CardQuartos onclick={handleClick} titleFont="font-Caveat" titleLabel="Eventos" backgroundColor="bg-blue-400" label="Desfrute do conforto e elegância da nossa suíte, projetada para proporcionar momentos inesquecíveis. Relaxe em um ambiente sofisticado, com todas as comodidades que você merece."/>
+            
         </main>
     );
 }
